cd86f736cd9170c0b1c59a221582db89e692f288
Tarek Saier <tareksaier@gmail.com>
2019-01-04 23:15:18 +0100

    fix coub download script

--- .scripts/coub-dl.sh
+++ .scripts/coub-dl.sh
@@ -3,6 +3,6 @@ tmp_title=`uuidgen`
 youtube-dl -o "${tmp_title}.mp4" "$1"
 youtube-dl -f html5-audio-high -o "${tmp_title}.mp3" "$1"
 printf '\x00\x00' | dd of="${tmp_title}.mp4" bs=1 count=2 conv=notrunc
-title=`youtube-dl --get-title "https://coub.com/view/1jh670"`
+title=`youtube-dl --get-title "$1"`
 ffmpeg -hide_banner -i "${tmp_title}.mp4" -i "${tmp_title}.mp3" -c copy -shortest -movflags faststart "${title}".mp4
 rm "${tmp_title}.mp4" "${tmp_title}.mp3"
